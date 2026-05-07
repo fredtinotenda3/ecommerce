c2437168cb8eeab1628810e6b44aa9382a5c4adc
fredtinotenda3 <fredtinotenda3.com>
2026-05-07 13:08:23 +0200

    feat production

--- src/app/_api/shared.ts
+++ src/app/_api/shared.ts
@@ -1,3 +1,2 @@
-export const GRAPHQL_API_URL = process.env.NEXT_BUILD
-  ? `http://127.0.0.1:${process.env.PORT || 3000}`
-  : process.env.NEXT_PUBLIC_SERVER_URL
+export const GRAPHQL_API_URL = process.env.NEXT_PUBLIC_SERVER_URL || 
+  (process.env.NEXT_BUILD ? process.env.NEXT_PUBLIC_SERVER_URL : 'http://localhost:3000')
