c1c078b1b8bb65c8e8b82d4f998a6458978db4b3
fredtinotenda3 <fredtinotenda3.com>
2026-05-08 10:08:48 +0200

    Remove Payload Cloud plugin

--- src/payload/payload.config.ts
+++ src/payload/payload.config.ts
@@ -1,6 +1,6 @@
 import { webpackBundler } from '@payloadcms/bundler-webpack' // bundler-import
 import { mongooseAdapter } from '@payloadcms/db-mongodb' // database-adapter-import
-import { payloadCloud } from '@payloadcms/plugin-cloud'
+// import { payloadCloud } from '@payloadcms/plugin-cloud'
 // import formBuilder from '@payloadcms/plugin-form-builder'
 import nestedDocs from '@payloadcms/plugin-nested-docs'
 import redirects from '@payloadcms/plugin-redirects'
@@ -45,11 +45,7 @@ export default buildConfig({
     user: Users.slug,
     bundler: webpackBundler(), // bundler-config
     components: {
-      // The `BeforeLogin` component renders a message that you see while logging into your admin panel.
-      // Feel free to delete this at any time. Simply remove the line below and the import `BeforeLogin` statement on line 15.
       beforeLogin: [BeforeLogin],
-      // The `BeforeDashboard` component renders the 'welcome' block that you see after logging into your admin panel.
-      // Feel free to delete this at any time. Simply remove the line below and the import `BeforeDashboard` statement on line 15.
       beforeDashboard: [BeforeDashboard],
     },
     webpack: config => {
@@ -75,27 +71,37 @@ export default buildConfig({
       }
     },
   },
+
   editor: slateEditor({}), // editor-config
+
   // database-adapter-config-start
   db: mongooseAdapter({
     url: process.env.DATABASE_URI,
   }),
   // database-adapter-config-end
+
   serverURL: process.env.PAYLOAD_PUBLIC_SERVER_URL,
+
   collections: [Pages, Products, Orders, Media, Categories, Users],
+
   globals: [Settings, Header, Footer],
+
   typescript: {
     outputFile: path.resolve(__dirname, 'payload-types.ts'),
   },
+
   graphQL: {
     schemaOutputFile: path.resolve(__dirname, 'generated-schema.graphql'),
   },
+
   cors: ['https://checkout.stripe.com', process.env.PAYLOAD_PUBLIC_SERVER_URL || ''].filter(
     Boolean,
   ),
+
   csrf: ['https://checkout.stripe.com', process.env.PAYLOAD_PUBLIC_SERVER_URL || ''].filter(
     Boolean,
   ),
+
   endpoints: [
     {
       path: '/create-payment-intent',
@@ -112,38 +118,43 @@ export default buildConfig({
       method: 'get',
       handler: productsProxy,
     },
-    // The seed endpoint is used to populate the database with some example data
-    // You should delete this endpoint before deploying your site to production
+
+    // Seed endpoint
     {
       path: '/seed',
       method: 'get',
       handler: seed,
     },
   ],
+
   plugins: [
     // formBuilder({}),
+
     stripePlugin({
       stripeSecretKey: process.env.STRIPE_SECRET_KEY || '',
       isTestKey: Boolean(process.env.PAYLOAD_PUBLIC_STRIPE_IS_TEST_KEY),
       stripeWebhooksEndpointSecret: process.env.STRIPE_WEBHOOKS_SIGNING_SECRET,
       rest: false,
+
       webhooks: {
         'product.created': productUpdated,
         'product.updated': productUpdated,
         'price.updated': priceUpdated,
       },
     }),
+
     redirects({
       collections: ['pages', 'products'],
     }),
+
     nestedDocs({
       collections: ['categories'],
     }),
+
     seo({
       collections: ['pages', 'products'],
       generateTitle,
       uploadsCollection: 'media',
     }),
-    payloadCloud(),
   ],
 })
