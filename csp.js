15b128c4328497c057ddf804ef569e942ddd1342
fredtinotenda3 <fredtinotenda3.com>
2026-05-08 13:29:20 +0200

    feat "cjsp.js"

--- csp.js
+++ csp.js
@@ -34,3 +34,24 @@ module.exports = Object.entries(policies)
     return ''
   })
   .join('; ')
+
+const { NEXT_PUBLIC_SERVER_URL } = process.env
+
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' https://js.stripe.com https://maps.googleapis.com;
+  child-src https://js.stripe.com;
+  style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
+  font-src 'self' https://fonts.gstatic.com;
+  frame-src https://js.stripe.com https://hooks.stripe.com;
+  connect-src 'self' ${
+    NEXT_PUBLIC_SERVER_URL || ''
+  } https://checkout.stripe.com https://api.stripe.com https://maps.googleapis.com https://vitals.vercel-insights.com;
+  img-src 'self' data: https: blob:;
+  media-src 'self';
+  object-src 'none';
+`
+  .replace(/\s{2,}/g, ' ')
+  .trim()
+
+module.exports = ContentSecurityPolicy
