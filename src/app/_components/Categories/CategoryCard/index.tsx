46a04307beee6afc328e99602f1986525c351cd2
fredtinotenda3 <fredtinotenda3@gmail.com>
2026-08-25 11:45:50 +0200

    fix: handle null category media in CategoryCard

--- src/app/_components/Categories/CategoryCard/index.tsx
+++ src/app/_components/Categories/CategoryCard/index.tsx
@@ -2,7 +2,7 @@
 import React from 'react'
 import Link from 'next/link'
 
-import { Category } from '../../../../payload/payload-types'
+import { Category, Media } from '../../../../payload/payload-types'
 import { useFilter } from '../../../_providers/Filter'
 
 import classes from './index.module.scss'
@@ -12,14 +12,15 @@ type CategoryCardProps = {
 }
 
 const CategoryCard = ({ category }: CategoryCardProps) => {
-  const media = category.media as Media
+  const media = category.media as Media | string | null | undefined
+  const mediaUrl = media && typeof media === 'object' ? media.url : undefined
   const { setCategoryFilters } = useFilter()
 
   return (
     <Link
       href="/products"
       className={classes.card}
-      style={{ backgroundImage: `url(${media.url})` }}
+      style={{ backgroundImage: mediaUrl ? `url(${mediaUrl})` : undefined }}
       onClick={() => setCategoryFilters([category.id])}
     >
       <p className={classes.title}>{category.title}</p>
